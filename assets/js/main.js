aaa23cf7edf9fe925274c1fd1da863b37a93d9b9
startedHere <ugwu.chukwuma@outlook.com>
2023-05-10 16:50:44 +0100

    Added "Home" and "About" Sections to the main page...

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,13 +18,26 @@
 // 		navMenu.classList.remove('show-menu');
 // 	});
 // }
+
+// ! Display Mobile Nav Menu ....
+//////////////////////////////////////////////////////////
 const navMenu = document.getElementById('nav-menu');
 const menuToggle = document.getElementById('animated-toggle-button');
 const menuCheckbox = document.getElementById('hamburger');
+
+const navLink = document.querySelectorAll('.nav__link');
 
 if (menuToggle) {
 	menuToggle.addEventListener('click', () => {
 		menuCheckbox.checked = !menuCheckbox.checked;
 		navMenu.classList.toggle('show-menu');
+
+		//!_Hide Mobile Nav Menu When You Click Any Nav-Links...
+		navLink.forEach(n => {
+			n.addEventListener('click', () => {
+				menuCheckbox.checked = false;
+				navMenu.classList.remove('show-menu');
+			});
+		});
 	});
 }
